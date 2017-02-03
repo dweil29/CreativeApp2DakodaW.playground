94065eda8020b0149e54a7c1097095c0f8214ab5
dweil29 <dweil@students.mchenry.edu>
2017-02-03 10:22:55 -0600

    Critical Thinking Exercise 2
    This is my creative critical thinking exercise.

---
+++ Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// This app will utilize variables and operators to do something creative.
+
+
+// This is where I will initialize variables
+
+var a = 1; var b = 2; var c = 3; var d = 4; var e = 5; var f = 6; var g = 7; var h = 8
+var i = 9; var j = 10; var k = 11; var l = 12; var m = 13; var n = 14; var o = 15; var p = 16
+var q = 17; var r = 18; var s = 19; var t = 20; var u = 21; var v = 22; var w = 23; var x = 24
+var y = 25; var z = 26
+
+
+// This is where I calculate the total number corresponding to a name
+
+var myName = d + a + k + o + d + a
+
+var myMiddle = k + l + a + u + s
+
+var myLast = w + e + i + l
+
+var exercise = c + r + i + t + i + c + a + l + t + h + i + n + k + i + n + g
+
+// This is where I print out the total of my name
+
+print("This project assigns a number corresponding to each letter in the alphabet.")
+
+print("It then calculates the total of a few variables.")
+
+print("The total of my name is ", myName + myMiddle + myLast)
+
+print("Critical thinking equals", exercise)
